8d8f97bdff4757e8a1a73fe5c69853a9c594bc78
TonkyCZ <kinot.honzejk@gmail.com>
2021-12-11 18:34:48 +0100

    mostly modeled, I hope

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 #include <simlib.h>
 
 
-#define ENDTime = 365;
+#define ENDTime = 365
+#define year_demand = 328500
+#define day = 1
 const int max_d_vyroba = 900;
+int cipy_v_zasobe = 6300;
+int soucastky_v_zasobe = 6300;
+int denni_poptavka = 0;
+int vyrobeno_sklad = 900;
 
 Store Vyroba("Vyroba",max_d_vyroba);
 
@@ -19,9 +25,64 @@ class vyrobna : public Process{
     }
 };
 
+class new_parts : public Process{
+    void Behavior(){
+        Wait(day);
+        cipy_v_zasobe += max_d_vyroba;
+        soucastky_v_zasobe += max_d_vyroba;
+    }
+    public: new_parts(){
+        Activate();
+    }
+};
+
+class gen_parts : public Event{
+    void Behavior(){
+        new new_parts;
+        Activate(Time+day);
+    }
+    public: gen_parts(){
+        Activate();
+    }
+};
+
+class demand : public Process{
+    void Behavioral(){
+        denni_poptavka+=max_d_vyroba;
+        Wait(day);
+    }
+    public: demand(){
+        Activate();
+    }
+};
+
+class gen_demand : public Event{
+    void Behavior(){
+        new demand;
+        Activate(Time+day);
+    }
+    public: gen_demand(){
+        Activate();
+    }
+};
+
+
+//not done
+class demand_un_satisfied : public Process{
+    void Behavior(){
+        if(vyrobeno_sklad > 0){
+            vyrobeno_sklad--;
+        }
+        else{
+            Wait()
+        }
+    }
+};
+
 int main(int argc, char **argv){
 
 
 
     return EXIT_SUCCESS;
 }
+
