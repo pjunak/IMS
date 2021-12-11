85b45169d28508335213a626addce41a51723fdf
TonkyCZ <kinot.honzejk@gmail.com>
2021-12-11 23:03:07 +0100

    ping

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <simlib.h>
+#include <iostream>
 
 
 #define ENDTime 364
@@ -17,18 +18,22 @@ Store Vyroba("Vyroba",max_d_vyroba);
 
 class vyrobna : public Process{
     void Behavior(){
-
 		int denni_vyroba = 0;
 		while(cipy_v_zasobe > 0 && soucastky_v_zasobe > 0 && denni_vyroba < 900)
-		{
-			cipy_v_zasobe-=1;
-			soucastky_v_zasobe-=1;
-			denni_vyroba+=1;
+		{   
+            if(cipy_v_zasobe < max_d_vyroba){
+                denni_vyroba = cipy_v_zasobe;
+            }
+            else{
+                denni_vyroba = max_d_vyroba;
+            }
+            Enter(Vyroba,denni_vyroba);
+            Wait(DAY);
+            Leave(Vyroba,denni_vyroba);
+			cipy_v_zasobe-=denni_vyroba;
+			soucastky_v_zasobe-=denni_vyroba;
+			vyrobeno_sklad+=denni_vyroba;
 		}
-		Enter(Vyroba,denni_vyroba);
-        Wait(DAY);
-        Leave(Vyroba,denni_vyroba);
-		denni_vyroba = 0;
     }
     public: vyrobna(){
         Activate();
@@ -48,7 +53,6 @@ class gen_vyrobna : public Event{
 class new_parts : public Process{
     void Behavior(){
         Wait(DAY);
-        cipy_v_zasobe += 900;
         soucastky_v_zasobe += max_d_vyroba;
     }
     public: new_parts(){
@@ -62,6 +66,31 @@ class gen_parts : public Event{
         Activate(Time+DAY);
     }
     public: gen_parts(){
+        Activate();
+    }
+};
+
+class new_chips : public Process{
+    int dt;
+    void Behavior(){
+        Wait(DAY);
+        soucastky_v_zasobe += dt;
+        
+    }
+    public: new_chips(int d){
+        dt = d;
+        Activate();
+    }
+};
+
+class gen_chips : public Event{
+    int dt;
+    void Behavior(){
+        new new_chips(dt);
+        //std::cout << dt << std::endl;
+        Activate(Time+DAY);
+    }
+    public: gen_chips(int d): dt(d){
         Activate();
     }
 };
@@ -90,17 +119,17 @@ class customer_satisfied : public Process{
     void Behavior(){
         int timer = 0;
         time_t max_time;
-        max_time = Exponential(90);
-        while(timer < max_time){
+        max_time = Exponential(30);
+        while(timer <= max_time){
             if(vyrobeno_sklad < 50){
                 Wait(DAY);
+                timer += DAY;
             }
             else{
                 satisfied += 1;
                 vyrobeno_sklad -=50;
                 break;
             }
-            timer += DAY;
         }
         if(timer > max_time){
             unsatisfied += 1;
@@ -129,6 +158,7 @@ int main(int argc, char **argv){
     }
     new gen_vyrobna;
     new gen_parts;
+    new gen_chips(900);
     Run();
     Vyroba.Output();
     printf("Satisfied: %d\nUnsatisfied: %d\n",satisfied,unsatisfied);
