ccf59e7dac4c806bbf4e26294b3c3b5b910dd3bb
TonkyCZ <kinot.honzejk@gmail.com>
2021-12-11 19:10:57 +0100

    something done

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 #include <simlib.h>
 
 
-#define ENDTime = 365
-#define year_demand = 328500
-#define day = 1
+#define ENDTime 364
+#define DAY_demand 900
+#define DAY 1.0
 const int max_d_vyroba = 900;
 int cipy_v_zasobe = 6300;
 int soucastky_v_zasobe = 6300;
 int denni_poptavka = 0;
 int vyrobeno_sklad = 900;
+int satisfied = 0;
+int unsatisfied = 0;
 
 Store Vyroba("Vyroba",max_d_vyroba);
 
 class vyrobna : public Process{
-    double Arrival;
-    void Behavior(){
+    void Behavior(){            
         Enter(Vyroba,900);
-        Wait(1);
+        Wait(DAY);
         Leave(Vyroba,900);
     }
     public: vyrobna(){
@@ -25,9 +26,19 @@ class vyrobna : public Process{
     }
 };
 
+class gen_vyrobna : public Event{
+    void Behavior(){
+        new vyrobna();
+        Activate(Time+DAY);
+    }
+    public: gen_vyrobna(){
+        Activate();
+    }
+};
+
 class new_parts : public Process{
     void Behavior(){
-        Wait(day);
+        Wait(DAY);
         cipy_v_zasobe += max_d_vyroba;
         soucastky_v_zasobe += max_d_vyroba;
     }
@@ -39,7 +50,7 @@ class new_parts : public Process{
 class gen_parts : public Event{
     void Behavior(){
         new new_parts;
-        Activate(Time+day);
+        Activate(Time+DAY);
     }
     public: gen_parts(){
         Activate();
@@ -47,9 +58,9 @@ class gen_parts : public Event{
 };
 
 class demand : public Process{
-    void Behavioral(){
-        denni_poptavka+=max_d_vyroba;
-        Wait(day);
+    void Behavior(){
+        denni_poptavka+=DAY_demand;
+        Wait(DAY);
     }
     public: demand(){
         Activate();
@@ -59,7 +70,7 @@ class demand : public Process{
 class gen_demand : public Event{
     void Behavior(){
         new demand;
-        Activate(Time+day);
+        Activate(Time+DAY);
     }
     public: gen_demand(){
         Activate();
@@ -70,18 +81,45 @@ class gen_demand : public Event{
 //not done
 class demand_un_satisfied : public Process{
     void Behavior(){
-        if(vyrobeno_sklad > 0){
-            vyrobeno_sklad--;
+        int timer = 0;
+        time_t max_time;
+        max_time = Exponential(30);
+        while(timer < max_time){
+            if(vyrobeno_sklad == 0){
+                Wait(DAY);
+            }
+            else{
+                satisfied += 1;
+                break;
+            }
+            timer += DAY;
         }
-        else{
-            Wait()
+        if(timer > max_time){
+            unsatisfied += 1;
         }
+    }
+    public: demand_un_satisfied(){
+        Activate();
+    }
+};
+
+class gen_demand_un_satisfied : public Event{
+    void Behavior(){
+        new demand_un_satisfied;
+        Activate(Time+DAY);
     }
 };
 
 int main(int argc, char **argv){
-
-
+    Init(0,356);
+    new gen_demand;
+    for(int i=0; i< DAY_demand;i++){
+        new gen_demand_un_satisfied;
+    }
+    new gen_vyrobna;
+    new gen_parts;
+    Run();
+    Vyroba.Output();
 
     return EXIT_SUCCESS;
 }
