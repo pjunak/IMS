75d13030a6d6411f36160363aad0b58080eb2a30
TonkyCZ <kinot.honzejk@gmail.com>
2021-12-11 16:23:39 +0100

    I dont know how

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 
 #include <simlib.h>
 
-const int kapacita_skladu;
 
-Store Linka('S');
-/* pro puzití skladu v kodu potom 
-Enter(S,kapacita_skladu);
-Wait(1);
-Leave(S,kapacita_skladu);
-*/
+#define ENDTime = 365;
+const int max_d_vyroba = 900;
+
+Store Vyroba("Vyroba",max_d_vyroba);
+
+class vyrobna : public Process{
+    double Arrival;
+    void Behavior(){
+        Enter(Vyroba,900);
+        Wait(1);
+        Leave(Vyroba,900);
+    }
+    public: vyrobna(){
+        Activate();
+    }
+};
+
+int main(int argc, char **argv){
+
+
+
+    return EXIT_SUCCESS;
+}
