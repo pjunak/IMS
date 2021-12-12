eec12a2b9bfb19dff561be477ac831361a856f20
pjunak <junakpetr@gmail.com>
2021-12-12 17:18:52 +0100

    Výpisy a drobné úpravy

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <simlib.h>
-#include <iostream>
 #include <cmath>
 
 #define ENDTime 364
-#define NUBER_of_customers 18
 #define DAY 1.0
+
 const int max_d_vyroba = 900;
 int cipy_v_zasobe = 6300;
 int soucastky_v_zasobe = 6300;
@@ -43,16 +42,20 @@ class customer : public Process{
             prodano_menicu+=50;
 			satisfied += 1;
         }
-        for( Queue::iterator p = Fronta.begin(); p != Fronta.end(); ++p){
+        for( Queue::iterator p = Fronta.begin(); p != Fronta.end(); ++p)
+		{
             customer *z = (customer*)(*p);
-            if(vyrobeno_sklad > 50 && z != nullptr){
+            if(vyrobeno_sklad >= 50){
                 z->Out();
                 delete z->timeout;
                 vyrobeno_sklad-=50;
                 prodano_menicu+=50;
 				satisfied += 1;
                 z->Activate();
-            }
+            } else
+			{
+				break;
+			}
         }
     }
     public: customer(int t){
@@ -176,17 +179,24 @@ class gen_chips : public Event{
 };
 
 int main(int argc, char **argv){
-    Init(0,ENDTime);
+    Init(0, (argc > 1) ? atoi(argv[1]) : 365);
 	RandomSeed(time(NULL));
-	new gen_customer(atoi(argv[1]), atoi(argv[2]));
     new gen_vyrobna;
     new gen_parts;
-    new gen_chips(300, 0);
-	new gen_chips(600, atoi(argv[3]));
+    new gen_chips((argc > 3) ? 300 : 900, 0);
+	new gen_customer((argc > 2) ? atoi(argv[2]) : 18, (argc > 3) ? atoi(argv[3]) : 90);
+	if(argc > 3)
+	{
+		new gen_chips(600, (argc > 4) ? atoi(argv[4]) : 60);
+	}
     Run();
     Vyroba.Output();
     Fronta.Output();
-    printf("Satisfied: %d\nUnsatisfied: %d\n",satisfied,unsatisfied);
-	printf("Prodáno měničů: %d\n",prodano_menicu);
+	Print("Na konci obdobé zbylo na interních skladech:\n%d čipů\n%d ostatních součástek\n%d neprodaných měničů\n", cipy_v_zasobe, soucastky_v_zasobe, vyrobeno_sklad);
+    Print("Satisfied: %d\nUnsatisfied: %d\n",satisfied,unsatisfied);
+	Print("Prodáno měničů: %d\n",prodano_menicu);
+	Print("Odhadovaný přibližný obrat z prodeje: %d Euro\n",prodano_menicu*8000/25);
+	Print("Odhadovaná přibližná výše hrubého zisku podle marže z prodeje: %d\n",(prodano_menicu*8000/25)/100*23);
+	Print("Odhadované přibližné ztráty hrubého zisku způsobené nedodáním zboží: %d Euro\n",(unsatisfied*((argc > 2) ? atoi(argv[2]) : 18)*8000/25)/100*23);
     return EXIT_SUCCESS;
 }
